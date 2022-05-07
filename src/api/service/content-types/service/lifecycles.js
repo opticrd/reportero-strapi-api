a7e7dc551b4a5669369dcef980e257fd7617ee53
luismts <deivi_0326@hotmail.com>
2022-05-07 16:47:29 -0400

    Add update log service

--- src/api/service/content-types/service/lifecycles.js
+++ src/api/service/content-types/service/lifecycles.js
@@ -4,16 +4,35 @@ module.exports =
 { 
     afterCreate(event) 
     {       
-        const { result, params  } = event;
+        const { result, params, model   } = event;
 
         let author = strapi.config.helpers.removeAuthorCredentials(result.createdBy);
 
         strapi.service('api::log.log').create({
             data: {
-                model: 'service',
+                model: model.singularName,
                 action: 'CREATE',
                 content: params.data,
                 timestamp: result.createdAt,
+                origin_id: result.id.toString(),
+                type: 'INFO',
+                author: author,
+            }
+        });
+    },
+    afterUpdate(event) 
+    {       
+        console.log(event);
+        const { result, params, model  } = event;
+
+        let author = strapi.config.helpers.removeAuthorCredentials(result.updatedBy);
+
+        strapi.service('api::log.log').create({
+            data: {
+                model: model.singularName,
+                action: 'UPDATE',
+                content: params.data,
+                timestamp: result.updatedAt,
                 origin_id: result.id.toString(),
                 type: 'INFO',
                 author: author,
