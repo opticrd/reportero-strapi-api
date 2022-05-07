0179dcdfde722c81584ad6a914cbb49d5cf056e4
luismts <deivi_0326@hotmail.com>
2022-05-07 00:22:33 -0400

    Update database file

--- config/database.js
+++ config/database.js
@@ -1,17 +1,16 @@
 module.exports = ({ env }) => 
 {
   console.log(process.env);
-  const port = process.env['DATABASE_PORT'] && parseInt(process.env['DATABASE_PORT']);
 
   return {
     connection: {
       client: 'mysql',
       connection: {
-        host: process.env['DATABASE_HOST'] || '104.196.187.29',
-        port: port || 3306,
+        host: process.env['DATABASE_HOST'] || 'localhost',
+        port: process.env['DATABASE_PORT'] || 3306,
         database: process.env['DATABASE_NAME'] || 'reportero',
         user: process.env['DATABASE_USERNAME'] || 'root',
-        password: process.env['DATABASE_PASSWORD'] || 'BoezBxeFKhBk0ztk',
+        password: process.env['DATABASE_PASSWORD'] || '',
         ssl: process.env['DATABASE_SSL'] || false,
       },
     },
