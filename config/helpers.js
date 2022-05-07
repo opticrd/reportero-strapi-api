b4fe370fd9df21a3327d6edf93f921b8875138e8
luismts <deivi_0326@hotmail.com>
2022-05-07 17:25:09 -0400

    Add a log activity helper

--- config/helpers.js
+++ config/helpers.js
@@ -9,4 +9,22 @@ module.exports =
 
         return author;
     },
+    logActivity(event, action, type = 'INFO') 
+    {
+        const { result, params, model  } = event;
+
+        let author = strapi.config.helpers.removeAuthorCredentials(result.updatedBy);
+
+        strapi.service('api::log.log').create({
+            data: {
+                model: model.singularName,
+                action: action,
+                content: params.data,
+                timestamp: new Date(),
+                origin_id: result.id.toString(),
+                type: type,
+                author: author,
+            }
+        });
+    },
 };
--- src/api/service/content-types/service/lifecycles.js
+++ src/api/service/content-types/service/lifecycles.js
@@ -3,40 +3,15 @@
 module.exports = 
 { 
     afterCreate(event) 
-    {       
-        const { result, params, model   } = event;
-
-        let author = strapi.config.helpers.removeAuthorCredentials(result.createdBy);
-
-        strapi.service('api::log.log').create({
-            data: {
-                model: model.singularName,
-                action: 'CREATE',
-                content: params.data,
-                timestamp: result.createdAt,
-                origin_id: result.id.toString(),
-                type: 'INFO',
-                author: author,
-            }
-        });
+    {      
+        strapi.config.helpers.logActivity(event, 'CREATE');
     },
     afterUpdate(event) 
     {       
-        console.log(event);
-        const { result, params, model  } = event;
-
-        let author = strapi.config.helpers.removeAuthorCredentials(result.updatedBy);
-
-        strapi.service('api::log.log').create({
-            data: {
-                model: model.singularName,
-                action: 'UPDATE',
-                content: params.data,
-                timestamp: result.updatedAt,
-                origin_id: result.id.toString(),
-                type: 'INFO',
-                author: author,
-            }
-        });
+        strapi.config.helpers.logActivity(event, 'UPDATE');
+    },
+    afterDelete(event) 
+    {       
+        strapi.config.helpers.logActivity(event, 'DELETE');
     },
 };
