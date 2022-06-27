2e1994c04d96df7ad42a7331ef07b83e893eda8a
luismts <deivi_0326@hotmail.com>
2022-06-27 16:42:22 -0400

    Update GOOGLE_BUCKET_NAME variable name

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       provider: 'strapi-provider-upload-google-cloud-storage',
       providerOptions: {
         serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
-        bucketName: env('GCS_BUCKET_NAME'),
+        bucketName: env('GOOGLE_BUCKET_NAME'),
         baseUrl: env('GCS_BASE_URL'),
       },
     },
